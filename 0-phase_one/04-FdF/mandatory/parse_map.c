8cf7cd96059e644888ad1578f99f02b9e6353b02
Lucas de Souza <110189896+LukkazZ12@users.noreply.github.com>
2023-10-10 14:51:08 -0300

    tirei o cálculo da escala dos whiles.

--- 0-phase_one/04-FdF/mandatory/parse_map.c
+++ 0-phase_one/04-FdF/mandatory/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: lucade-s <lucade-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 11:46:36 by lucade-s          #+#    #+#             */
-/*   Updated: 2023/03/27 11:46:36 by lucade-s         ###   ########.fr       */
+/*   Updated: 2023/10/10 14:25:03 by lucade-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ static void	read_z(t_data *data, char **split)
 
 static void	apply_scale(t_data *data)
 {
-	data->scale = WIN_HEIGHT / hypot(data->rows, data->cols);
 	data->map[data->aux_rows][data->aux_cols].y
 		= data->aux_rows * data->scale;
 	data->map[data->aux_rows][data->aux_cols].x
@@ -71,6 +70,7 @@ void	parse_map(t_data *data)
 	data->aux_rows = 0;
 	data->aux_cols = 0;
 	data->fd = open(data->map_name, O_RDONLY);
+	data->scale = WIN_HEIGHT / hypot(data->rows, data->cols);
 	while (data->aux_rows < data->rows)
 	{
 		data->res = get_next_line(data->fd);
