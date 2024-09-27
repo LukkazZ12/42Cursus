a06279161b6b992b4535733768cf6375cc21a282
Lucas <lucas.souza.santos12@outlook.com>
2024-09-27 16:48:06 -0300

    .

--- 2-phase_three/21-inception/srcs/requirements/wordpress/conf/wp-config.php
+++ 2-phase_three/21-inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -4,16 +4,14 @@ define( 'DB_USER', getenv('SQL_USER') );
 define( 'DB_PASSWORD', getenv('SQL_PASSWORD') );
 define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', '');
 
 $table_prefix = 'wp_';
 
 define( 'WP_DEBUG', false );
 
-if ( ! defined( 'ABSPATH' ) )
-{
-	define( 'ABSPATH', __DIR__ . '/' );
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
 }
 
 require_once ABSPATH . 'wp-settings.php';
-?>
